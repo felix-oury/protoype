f6ba1c5bcc645abdbdb3ff2633ede717feb6d07d
“felix-oury” <felix.oury19@imperial.ac.uk>
2021-12-09 11:52:44 +0000

    LoginPanel

--- src/main/java/LoginPanel.java
+++ src/main/java/LoginPanel.java
@@ -1,50 +1,86 @@
 import javax.swing.*;
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.awt.event.*;
+import javax.swing.*;
 import java.io.IOException;
 
-public class LoginPanel extends JPanel {
 
-    public JPanel LoginPanel;
-    public JPanel Logo;
-    public JPanel Login;
+public class LoginPanel extends JPanel implements ActionListener{
 
-    public JTextArea textUsername;
-    public JTextArea textPassword;
-    public JLabel username;
-    public JLabel password;
+    public JPanel LoginPanel,LogoPanel,Login;
+    public FlowLayout grid;
+
+    public JTextField textUsername;
+    public JPasswordField textPassword;
+    public JLabel username,password,message;
+    public JButton LoginButton;
     public Canvas drawing;
 
     public LoginPanel() throws IOException{
-        LoginPanel = new JPanel(new GridLayout(2,1));
+        LoginPanel = new JPanel();
         LoginPanel.setBackground(Color.WHITE);
+        LogoPanel = new JPanel();
+        Login =new JPanel(new FlowLayout());
+        LoginButton = new JButton("Login");
+
+        message = new JLabel();
 
 
-        textUsername = new JTextArea(1,20);
-        textPassword = new JTextArea(1,20);
+        textUsername = new JTextField(20);
+        textPassword = new JPasswordField(20);
         username= new JLabel("<html>Username :<br><html>");
         password= new JLabel("Password :");
+        grid = new FlowLayout(10,10,10);
+
+
 
         drawing= new Canvas();
 
         BufferedImage myPicture = ImageIO.read(new File("/Users/felixoury/Desktop/docint.png"));
         JLabel logo = new JLabel(new ImageIcon(myPicture));
 
-        Logo.add(logo);
+
+        LogoPanel.add(logo);
         Login.add(username);
         Login.add(textUsername);
         Login.add(password);
         Login.add(textPassword);
+        Login.add(message);
 
-        LoginPanel.add(Logo);
+
+
+
+        LoginPanel.add(LogoPanel);
         LoginPanel.add(Login);
+        LoginPanel.add(LoginButton);
 
-        setLayout(new GridLayout());
+        LoginPanel.add(drawing);
+
+        setLayout(grid);
+
+
+
     }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        String usernameS = username.getText();
+        String passwordS =password.getText();
+        if(usernameS.trim().equals("Felix")&& passwordS.trim().equals("OK")){
+            message.setText(" Hello " + usernameS + "");
+        }
+        else{message.setText("invalid User");
+        }
+
+    }
+
     public JPanel getLoginPanel() {
         return LoginPanel;
     }
+
+
 
 }
--- src/main/java/SimpleUI.java
+++ src/main/java/SimpleUI.java
@@ -7,24 +7,26 @@ public class SimpleUI {
     public static void main(String[] args) throws IOException {
 
         JFrame frame = new JFrame(gc); // Create a new JFrame
-        frame.setSize(1200,600);
+        frame.setSize(700,450);
 
         LoginPanel LP = new LoginPanel();
         frame.getContentPane().add(LP);
         frame.add(LP.getLoginPanel());
-        frame.setJMenuBar(new MainMenu());
-        frame.setVisible(true);
-        frame.setVisible(true);
-
-
-
 
         //ButtonPanel DUIC =new ButtonPanel();
         //frame.getContentPane().add(DUIC);
         //frame.add(DUIC.getMainPanel());
 
 
+        frame.setJMenuBar(new MainMenu());
+        frame.setVisible(true);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+
+
+
 // This next line closes the program when the frame is closed
-    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-}
+    }
 }
